df040b62ffe15b1ce3eb6cafae1f7c2567df2630
carlos-07souza <karlo_ssouza@hotmail.com>
2025-05-25 15:00:23 -0300

    Add files via upload

---
+++ SobrePage.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppHotel.Views;
+
+public partial class SobrePage : ContentPage
+{
+	public SobrePage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
